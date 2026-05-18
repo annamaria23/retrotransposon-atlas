19458650782bfe34752acbd53f19ea90e6066b8b
annamaria23 <88196111+annamaria23@users.noreply.github.com>
2026-05-18 01:45:25 +0100

    Create dnds_m0.sh

---
+++ Main_Figures/dnds_m0.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+conda activate phylo
+
+FILENAME="$(basename "$1")"
+FILEPATH="$(dirname "$1")"
+
+mkdir -p ${FILEPATH}/260317_${FILENAME}.dnds
+cd ${FILEPATH}/260317_${FILENAME}.dnds
+
+scp ${FILEPATH}/${FILENAME}.dnds/aa.maf.phy .
+FastTree -nt -gtr aa.maf.phy > tree.nwk
+
+cat > codeml_m0.ctl << EOF
+seqfile  = aa.maf.phy
+treefile = tree.nwk
+outfile  = dnds.res.m0.out
+
+runmode = 0
+seqtype = 1
+CodonFreq = 2
+model = 0
+NSsites = 0
+fix_omega = 1
+omega = 1
+EOF
+
+codeml codeml_m0.ctl > 260317_codeml_m0.log
